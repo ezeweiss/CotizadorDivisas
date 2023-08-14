975754256d5b4acc31f3399a3f7b5369825b38d6
Ezequiel Weiss <38471904+ezeweiss@users.noreply.github.com>
2023-08-14 08:14:07 -0300

    agrego funciones de cotizar y conversion

--- js/calcularDivisa.js
+++ js/calcularDivisa.js
@@ -2,25 +2,48 @@ const tasasDeCambio = {
     dolar: {
         euro: 0.84,
         libra: 0.72,
-        ARS: 200
+        ARS: 600
     }
 }
 
-let continuar = true;
-
-while(continuar){
-const divisaOrigen = prompt("Introduce la divisa que quieres convertir: ");
-const divisaDestino = prompt("Introduce la divisa a la que quieres convertir: ");
-
-const cantidad = parseFloat(prompt("Introduce la cantidad que quieres convertir: "));
-
-if(tasasDeCambio[divisaOrigen] && tasasDeCambio[divisaOrigen][divisaDestino]){
-    const tasa = tasasDeCambio[divisaOrigen][divisaDestino];
-    const resultado = cantidad * tasa;
-    alert(`${cantidad} ${divisaOrigen} son ${resultado} ${divisaDestino}`);
-} else{
-    alert("La divisa origen o destino no es correcta");
+function realizarConversion(divisaOrigen, divisaDestino, cantidad){
+    if(tasasDeCambio[divisaOrigen] && tasasDeCambio[divisaOrigen][divisaDestino]){
+        const tasa = tasasDeCambio[divisaOrigen][divisaDestino];
+        const resultado = cantidad * tasa;
+        alert(`${cantidad} ${divisaOrigen} son ${resultado} ${divisaDestino}`);
+    } else{
+        alert("La divisa origen o destino no es correcta");
+    }
 }
 
-continuar = confirm("¿Quieres hacer otra conversión?");
+function cotizadorDivisas(){
+    let continuar = true;
+
+    while(continuar){
+    const divisaOrigen = prompt("Introduce la divisa que quieres convertir: ");
+    if (!divisaOrigen) {
+        alert("Debes ingresar una divisa.");
+        continue;
+    }
+
+    const divisaDestino = prompt("Introduce la divisa a la que quieres convertir: ");
+    if (!divisaDestino) {
+        alert("Debes ingresar una divisa.");
+        continue;
+    }
+
+    const cantidad = parseFloat(prompt("Introduce la cantidad que quieres convertir: "));
+    if (isNaN(cantidad)) {
+        alert("Debes ingresar un valor numérico.");
+        continue;
+    }
+
+    realizarConversion(divisaOrigen, divisaDestino, cantidad);
+
+    const respuesta = prompt("Quieres realizar otra conversión? (Si/No)");
+    continuar = respuesta === "si";
+    }
+    alert("Gracias por usar nuestro conversor de divisas");
 }
+
+cotizadorDivisas();
